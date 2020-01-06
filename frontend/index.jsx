37b5b29d8e10d47c246fd62f38eb2e06f8311d1a
Jared <jmarkowitz098@gmail.com>
2020-01-06 15:19:37 -0800

    Finish user auth Phase 1

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {login, logout, createUser} from './util/session_api_util';
+import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
+    const store = configureStore();
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to Keep</h1>, root);
 
@@ -10,4 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     window.createUser = createUser;
     window.login = login;
     window.logout = logout;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+
 });
---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions"
+
+const usersReducer = (state = {}, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            let newState = Object.assign({}, state);
+            newState[action.currentUser.id] = action.currentUser;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default usersReducer;
