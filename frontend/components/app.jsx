3d38e61c376c4f4292b3a1f35e7bf4ad5b8785f6
Jared <jmarkowitz098@gmail.com>
2020-01-06 17:06:42 -0800

    Add user auth final

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom'
 import GreetingContainer from "./greeting/greeting_container"
 import LoginFormContainer from './sessions/login_form_container'
 import SignupFormContainer from './sessions/create_user_form_container'
+import { AuthRoute } from '../util/route_util'
 
 const App = () => (
     <div>
@@ -10,8 +11,8 @@ const App = () => (
             <h1>KEEP GOING BABY</h1>
             <GreetingContainer />
         </header>
-        <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
+        <AuthRoute path="/login" component={LoginFormContainer} />
+        <AuthRoute path="/signup" component={SignupFormContainer} />
     </div>
 );
 
