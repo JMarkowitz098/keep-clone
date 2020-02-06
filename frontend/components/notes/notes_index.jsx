809ecee4c016379175f2e41acd3bd068987c313c
Jared <jmarkowitz098@gmail.com>
2020-02-06 09:40:53 -0800

    Switch branch

--- frontend/components/notes/notes_index.jsx
+++ frontend/components/notes/notes_index.jsx
@@ -1,56 +1,54 @@
-import React from 'react';
-import NoteIndexItem from './note_index_item'
-import CreateNoteContainer from './create_note_container'
+import React from "react";
+import NoteIndexItem from "./note_index_item";
+import CreateNoteContainer from "./create_note_container";
 
 class NotesIndex extends React.Component {
-    constructor (props){
-        super(props)
-       this.parentDeleteNote = this.parentDeleteNote.bind(this)
-       this.parentCreateNote = this.parentCreateNote.bind(this)
-    }
+	constructor(props) {
+		super(props);
+		this.parentDeleteNote = this.parentDeleteNote.bind(this);
+		this.parentCreateNote = this.parentCreateNote.bind(this);
+	}
 
-    componentDidMount() {
-        this.props.fetchNotes()
-    }
+	componentDidMount() {
+		this.props.fetchNotes();
+	}
 
-    parentDeleteNote(id) {
-        this.props.deleteNote(id)
-            .then(() => this.props.fetchNotes())
-    }
+	parentDeleteNote(id) {
+		this.props.deleteNote(id).then(() => this.props.fetchNotes());
+	}
 
-    parentCreateNote(id) {
-        this.props.createNote(id)
-            .then(() => this.props.fetchNotes())
-    }
+	parentCreateNote(id) {
+		this.props.createNote(id).then(() => this.props.fetchNotes());
+	}
 
-    sortNotes() {
-        return this.props.notes.sort((a,b) => a.position - b.position)
-    }
+	sortNotes() {
+		return this.props.notes.sort((a, b) => a.position - b.position);
+	}
 
-    renderNotes() {
-        let sortedNotes = this.sortNotes()
-        return sortedNotes.map((note) => (
-            <NoteIndexItem 
-                key={note.id}
-                note={note} 
-                position={note.position} 
-                deleteNote={this.parentDeleteNote}
-                openModal={this.props.openModal}
-                closeModal={this.props.closeModal}
-                history={this.props.history}
-            />
-        ))
-    }
+	renderNotes() {
+		let sortedNotes = this.sortNotes();
+		return sortedNotes.map(note => (
+			<NoteIndexItem
+				key={note.id}
+				note={note}
+				position={note.position}
+				deleteNote={this.parentDeleteNote}
+				openModal={this.props.openModal}
+				closeModal={this.props.closeModal}
+				history={this.props.history}
+			/>
+		));
+	}
 
-    render() {
-        return(
-        <div id="note-index-form-container">
-            <CreateNoteContainer id="create-note-form" />
-            <div id="list-container">
-                <ul id='notes-list'>{this.renderNotes()}</ul>
-            </div>
-
-        </div>)
-    }
+	render() {
+		return (
+			<div id="note-index-form-container">
+				<CreateNoteContainer id="create-note-form" />
+				<div id="list-container">
+					<ul id="notes-list">{this.renderNotes()}</ul>
+				</div>
+			</div>
+		);
+	}
 }
-export default NotesIndex
+export default NotesIndex;
--- frontend/components/notes/notes_index_container.js
+++ frontend/components/notes/notes_index_container.js
@@ -1,20 +1,20 @@
-import { connect } from 'react-redux';
-import NotesIndex from "../notes/notes_index"
-import { fetchNotes, deleteNote } from "../../actions/note_actions"
-import { createNote } from '../../util/note_api_util';
-import { openModal, closeModal } from "../../actions/modal_actions" 
-const mstp = (state) => {
-    return ({
-            notes: Object.values(state.entities.notes)
-    })
-}
+import { connect } from "react-redux";
+import NotesIndex from "../notes/notes_index";
+import { fetchNotes, deleteNote } from "../../actions/note_actions";
+import { createNote } from "../../util/note_api_util";
+import { openModal, closeModal } from "../../actions/modal_actions";
+const mstp = state => {
+	return {
+		notes: Object.values(state.entities.notes)
+	};
+};
 
-const mdtp = (dispatch) => ({
-    fetchNotes: () => dispatch(fetchNotes()),
-    deleteNote: (noteId) => dispatch(deleteNote(noteId)),
-    createNote: (note) => dispatch(createNote(note)),
-    openModal: (modal, note) => dispatch(openModal(modal,note)),
-    closeModal: () => dispatch(closeModal())
-})
+const mdtp = dispatch => ({
+	fetchNotes: () => dispatch(fetchNotes()),
+	deleteNote: noteId => dispatch(deleteNote(noteId)),
+	createNote: note => dispatch(createNote(note)),
+	openModal: (modal, note) => dispatch(openModal(modal, note)),
+	closeModal: () => dispatch(closeModal())
+});
 
-export default connect (mstp, mdtp)(NotesIndex)
+export default connect(mstp, mdtp)(NotesIndex);
