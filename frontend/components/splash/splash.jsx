aa8887fcb6a5a14ddd1f338c32fd6fb875d97ab9
Jared <jmarkowitz098@gmail.com>
2020-02-06 16:04:01 -0800

    Add video to splash page

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+
 class Splash extends React.Component {
+    constructor(props) {
+        super(props)
+    }
 
     render() {
         return (
-            <div>
-                
+            <div id="splash-container">
+                <video autoPlay loop muted>
+                    <source src={window.video}/>
+                </video>
+                <div id="splash-overlay">
+                    <h3>Google Keep Tribute</h3>
+                    <button
+                        onClick={() => this.props.history.push('/signup')}
+                    >Sign Up</button>
+                    <button 
+                        onClick={() => this.props.history.push('/login')}>
+                    Log In</button>
+                    <button>Use Demo Account</button>
+                </div>
             </div>
         )
     }
